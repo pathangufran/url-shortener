fe246de39aee99d6e68fccfaa4080a0dabfa2b90
Gufran <gufranpathann@gmail.com>
2026-08-09 00:01:31 +0530

    feat(analytics): implemented analytics model

--- apps/analytics/models.py
+++ apps/analytics/models.py
@@ -1,3 +1,33 @@
+from uuid import uuid4
 from django.db import models
+from apps.shortener.models import URL
 
-# Create your models here.
+class ClickEvent(models.Model):
+    """
+    Represents a single click on a shortened URL.
+    """
+
+    id = models.UUIDField(primary_key=True,default=uuid4,editable=False)
+    url = models.ForeignKey(URL,on_delete=models.CASCADE,
+                            related_name="click_events",)
+    ip_address = models.GenericIPAddressField(null=True,blank=True,)
+    country = models.CharField(max_length=100,null=True,blank=True,)
+    user_agent = models.TextField(null=True,blank=True,)
+    referrer = models.URLField(null=True,blank=True,)
+    browser = models.CharField(max_length=100,null=True,blank=True,)
+    device = models.CharField(max_length=100,null=True,blank=True,)
+    clicked_at = models.DateTimeField(auto_now_add=True,db_index=True,)
+
+    class Meta:
+        db_table = "click_events"
+
+        indexes = [
+            models.Index(fields=["url","-clicked_at"],),
+            models.Index(fields=["url","ip_address"],),
+            models.Index(fields=["country"],),
+            models.Index(fields=["browser"],),
+        ]
+        ordering = ["-clicked_at"]
+
+    def __str__(self):
+        return f"{self.url.short_code} - {self.clicked_at}"
